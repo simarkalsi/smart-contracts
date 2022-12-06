a96ea94b13ec45f85ec51d65ed37a5e4b4cde083
Simarjeet kalsi <singhsimar280@gmail.com>
2022-12-06 23:29:12 +0530

    Add files via upload

---
+++ NFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    uint public tokenCount;
+
+     constructor() ERC721("DApp NFT", "DAPP") {}
+
+     function mint(string memory _tokenURI) external returns (uint) {
+        tokenCount++;
+        _safeMint(msg.sender,tokenCount);
+        _setTokenURI(tokenCount,_tokenURI);
+         return(tokenCount);
+        
+     }
+
+}
